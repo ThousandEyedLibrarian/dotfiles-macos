ddca5daa68e2b0947ccb47320c5e07b7ed85998d
Librarian <carter@librarian.wiki>
2026-02-17 23:43:47 +1100

    chore: add omarchy style week and date swap on click for middle oriented time in sketchybar

--- .config/sketchybar/items/calendar.lua
+++ .config/sketchybar/items/calendar.lua
@@ -1,6 +1,8 @@
 local settings = require("settings")
 local colors = require("colors")
 
+local show_date = false
+
 -- Clock in center (omarchy style)
 local clock = sbar.add("item", "clock", {
   position = "center",
@@ -17,6 +19,19 @@ local clock = sbar.add("item", "clock", {
   update_freq = 10,
 })
 
-clock:subscribe({"forced", "routine", "system_woke"}, function(env)
-  clock:set({ label = os.date("%a %H:%M") })
+local function update_clock()
+  if show_date then
+    clock:set({ label = os.date("W%V %d %b %Y") })
+  else
+    clock:set({ label = os.date("%a %H:%M") })
+  end
+end
+
+clock:subscribe({"forced", "routine", "system_woke"}, function()
+  update_clock()
+end)
+
+clock:subscribe("mouse.clicked", function()
+  show_date = not show_date
+  update_clock()
 end)
